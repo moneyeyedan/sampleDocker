e07149fb4e76633a700a8c73585d6b36a668690e
manikandan <moneykandan995@gmail..com>
2020-03-01 08:10:25 +0530

    correct file of docker install link

--- src/app.js
+++ src/app.js
@@ -14,3 +14,5 @@ app.listen(5000,()=>{
 //instruction of the site link https://buddy.works/guides/how-dockerize-node-application
 //docker container run to=>docker run -p <docker=>yourwishport>:<local=>yourwishPort> <build dockername>
 //if you access the docker rest api you provide the http://localhost:<put docker port here>
+
+//https://linuxize.com/post/how-to-install-and-use-docker-on-ubuntu-18-04/
